aa8dc5a82fd0b7d6310a876e30a32f8bf56266a1
Abuzar Yaseen <abuzaryaseen88@gmail.com>
2023-09-15 22:42:08 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const calculateResult = (input) => {
+    try {
+      const operators = ["+", "-", "*", "/", "%"];
+      let operator = null;
+
+      for (let i = 0; i < input.length; i++) {
+        if (operators.includes(input[i])) {
+          operator = input[i];
+          break;
+        }
+      }
+      if (!operator) {
+        setInput(parseFloat(input).toString());
+        return;
+      }
+
+      const [operand1, operand2] = input.split(operator).map(parseFloat);
+
+      var result;
+
+      switch (operator) {
+        case "+":
+          result = operand1 + operand2;
+          break;
+        case "-":
+          result = operand1 - operand2;
+          break;
+        case "*":
+          result = operand1 * operand2;
+          break;
+        case "/":
+          result = operand1 / operand2;
+          break;
+        case "%":
+          result = operand1 % operand2;
+          break;
+        default:
+          throw new Error("invalid operator");
+      }
+      setInput(result.toString());
+    } catch (error) {
+      setInput("Error");
+    }
+  };
+
+  const handleClickButton = (value) => {
+    if (value === "C") {
+      setInput("");
+    } else if (value === "<") {
+      setInput(input.slice(0, -1));
+    } else if (value === "=") {
+      calculateResult(input);
+    } else {
+      setInput((preValue) => preValue + value);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="cal">
+        <h1 id="input">{input}</h1>
+        <div>
+          <button onClick={() => handleClickButton("C")}>C</button>
+          <button onClick={() => handleClickButton("<")}>&lt;</button>
+          <button onClick={() => handleClickButton("%")}>%</button>
+          <button onClick={() => handleClickButton("/")}>/</button>
+        </div>
+        <div>
+          <button onClick={() => handleClickButton("7")}>7</button>
+          <button onClick={() => handleClickButton("8")}>8</button>
+          <button onClick={() => handleClickButton("9")}>9</button>
+          <button onClick={() => handleClickButton("*")}>*</button>
+        </div>
+        <div>
+          <button onClick={() => handleClickButton("4")}>4</button>
+          <button onClick={() => handleClickButton("5")}>5</button>
+          <button onClick={() => handleClickButton("6")}>6</button>
+          <button onClick={() => handleClickButton("-")}>-</button>
+        </div>
+        <div>
+          <button onClick={() => handleClickButton("1")}>1</button>
+          <button onClick={() => handleClickButton("2")}>2</button>
+          <button onClick={() => handleClickButton("3")}>3</button>
+          <button onClick={() => handleClickButton("+")}>+</button>
+        </div>
+        <div>
+          <button onClick={() => handleClickButton("0")}>0</button>
+          <button onClick={() => handleClickButton("00")}>00</button>
+          <button onClick={() => handleClickButton(".")}>.</button>
+          <button onClick={() => handleClickButton("=")}>=</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
